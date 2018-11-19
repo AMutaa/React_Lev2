dabb410400b43d0931e8e6516789b73529f3296e
AMutaa <piusandru@gmail.com>
2018-11-19 15:50:16 -0500

    render_prop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Toggle from './Toggle';
+import Toggle from './ToggleRenderProps';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +7,12 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hello</h1>
-        <Toggle />
+        <Toggle render={({ on, toggle }) => (
+          <div>
+            {on && <h1>Show Me</h1>}
+            <button onClick={toggle}>Show / Hide</button>
+          </div>
+        )} />
       </div>
     );
   }
