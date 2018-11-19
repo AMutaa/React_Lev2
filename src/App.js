d7d3bdd0de1bdd51ed9b222c56cd8912a885de82
AMutaa <piusandru@gmail.com>
2018-11-19 17:03:49 -0500

    code refactor:

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Toggle from './ToggleRenderProps';
+import Toggle from './ToggleRenderPropChildren';
 import './App.css';
 
 class App extends Component {
@@ -7,14 +7,16 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hello</h1>
-        <Toggle render={({ on, toggle }) => (
+        <Toggle>
+          ({on, toggle}) => (
           <div>
             {on && <h1>Show Me</h1>}
             <button onClick={toggle}>Show / Hide</button>
           </div>
-        )} />
+          )
+        </Toggle>
       </div>
-    );
+    )
   }
 }
 
