b8d1301c7f528b68f13b3554d4ebba9d356be27d
AMutaa <piusandru@gmail.com>
2018-11-27 16:12:42 -0500

    icon

--- src/Utilities/Icon.js
+++ src/Utilities/Icon.js
@@ -4,8 +4,8 @@ export default class Icon extends Component {
   static defaultProps = {
     color: 'black'
   }
+
   render() {
-    // eslint-disable-next-line default-case
     switch (this.props.name) {
       case 'close':
         return (
@@ -18,13 +18,11 @@ export default class Icon extends Component {
             enableBackground="new 0 0 31.11 31.11"
           >
             <polygon
-
               fill={this.props.color}
               points="31.11,1.41 29.7,0 15.56,14.14 1.41,0 0,1.41 14.14,15.56 0,29.7 1.41,31.11 15.56,16.97   29.7,31.11 31.11,29.7 16.97,15.56 "
             />
           </svg>
-        );
+        )
     }
   }
 }
-
