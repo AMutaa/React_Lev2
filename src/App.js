1dcfb565654ec288f2232beb9aff4bd03ef0bea4
AMutaa <piusandru@gmail.com>
2018-11-27 15:16:47 -0500

    modal: Login Page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { Component, Fragment } from 'react';
 import Toggle from './Components/ToggleRenderPropChildren';
 import Portal from './Components/Portal';
+import Modal from './Components/Modal';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
         <Toggle>
           {
             ({ on, toggle }) => (
               <Fragment>
-                {on && <h1>Show Me</h1>}
                 <button onClick={toggle}>Login</button>
-                <Portal>{on && <h1>Hi, I'm in a portal</h1>}</Portal>
-
+                <Modal on={on} toggle={toggle}>
+                  <h1>Still In Modal: Login Page</h1>
+                </Modal>
               </Fragment>
             )}
         </Toggle>
