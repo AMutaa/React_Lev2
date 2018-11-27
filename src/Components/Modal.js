6611422fe5e6122cf978c93be341debb44ac0e2f
AMutaa <piusandru@gmail.com>
2018-11-27 15:52:17 -0500

    improving the modal

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -7,12 +7,16 @@ export default class Modal extends Component {
     const { children, on, toggle } = this.props
     return (
       <Portal>
-        {on && (
-          <ModalWrapper>
-            <button onClick={toggle}>Close</button>
-            <div>{children}</div>
-          </ModalWrapper>
-        )
+        {on &&
+          (
+            <ModalWrapper>
+              <ModalCard>
+                <CloseButton onClick={toggle}>Close</CloseButton>
+                <div>{children}</div>
+              </ModalCard>
+              <Background onClick={toggle} />
+            </ModalWrapper>
+          )
         }
       </Portal>
     )
@@ -26,6 +30,35 @@ const ModalWrapper = styled.div`
     left: 0;
     width:100%;
     height: 100%;
-    background-color:teal;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+`;
+
+const ModalCard = styled.div`
+position:relative;
+background: white;
+border-radius: 5px;
+padding:15px;
+box-shadow: 2px 2px 10px rgba(0,0,0,0.3);
+z-index:10;
+min-width:320px;
+margin-bottom:100px;
+`;
+
+const CloseButton = styled.button`
+position: absolute;
+top:0;
+right:0;
+`;
+
+const Background = styled.div`
+position:absolute;
+width:100%;
+height:100%;
+top:0;
+left:0;
+background:black;
+opacity:0.5;
 
 `;
