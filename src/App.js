f8cdd2832e50f93d84133ad0135c5d313760568d
AMutaa <piusandru@gmail.com>
2018-11-27 14:22:03 -0500

    creating a portal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import Toggle from './Components/ToggleRenderPropChildren';
-
+import Portal from './Components/Portal';
 import './App.css';
 
 class App extends Component {
@@ -11,12 +11,13 @@ class App extends Component {
           {
             ({ on, toggle }) => (
               <Fragment>
+                {on && <h1>Show Me</h1>}
                 <button onClick={toggle}>Login</button>
+                <Portal>{on && <h1>Hi, I'm in a portal</h1>}</Portal>
 
               </Fragment>
             )}
         </Toggle>
-
       </div >
     )
   }
