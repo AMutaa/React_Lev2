92d874479e330c5905e3c5016371bf162b46ef43
AMutaa <piusandru@gmail.com>
2018-11-28 16:12:56 -0500

    design utility transition

--- src/Elements/Cards.js
+++ src/Elements/Cards.js
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
-import { elevation } from 'Utilities'
+import { elevation, transition } from 'Utilities'
 
 export const Card = styled.div`
 background: white;
 border-radius: 5px;
 padding:15px;
 margin-bottom:100px;
-${elevation[4]}
+${elevation[4]};
+${transition({
+    ease: 'ease-in'
+  })};
+&: hover {
+  ${elevation[5]}
+}
 `;
--- src/Utilities/index.js
+++ src/Utilities/index.js
@@ -2,5 +2,6 @@
 import Portal from './Portal';
 import Toggle from './Toggle';
 import elevation from './elevation'
+import transition from './transition'
 
-export { Portal, Toggle, elevation };
+export { Portal, Toggle, elevation, transition };
