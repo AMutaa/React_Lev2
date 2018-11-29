0aaa28fb97158ff739e3b3c1a2bf7abfcb459dc1
AMutaa <piusandru@gmail.com>
2018-11-29 14:29:30 -0500

    context

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment, createContext } from 'react';
 import { Toggle } from 'Utilities';
 import { Modal } from 'Elements';
 import './App.css';
 
+const UserContext = createContext();
+
+class UserProvider extends Component {
+
+  state = {
+    id: '123',
+    name: 'Adam',
+    email: 'amutaa08@gmail.com'
+  }
+  render() {
+    return (
+      <UserContext.Provider
+        value={{
+          user: this.state
+        }}>
+        {this.props.children}
+      </UserContext.Provider >
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Toggle>
-          {
-            ({ on, toggle }) => (
-              <Fragment>
-                <button onClick={toggle}>Login</button>
-                <Modal on={on} toggle={toggle}>
-                  <h1>Still In Modal: Login Page</h1>
-                </Modal>
-              </Fragment>
-            )}
-        </Toggle>
-      </div >
+      <UserProvider>
+        <div className="App">
+          <Toggle>
+            {
+              ({ on, toggle }) => (
+                <Fragment>
+                  <button onClick={toggle}>Login</button>
+                  <Modal on={on} toggle={toggle}>
+                    <h1>Still In Modal: Login Page</h1>
+                  </Modal>
+                </Fragment>
+              )}
+          </Toggle>
+        </div >
+      </UserProvider>
     )
   }
 }
